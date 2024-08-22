7d16a95dcc136e87b2df9ca3a92acc3233698b2e
eusmas <eus08@hotmail.com>
2024-08-21 23:16:22 -0500

    sdf

--- Test_UDP.py
+++ Test_UDP.py
@@ -1,17 +1,22 @@
 import socket
-
-# Definir la dirección IP y el puerto al que se va a enviar el mensaje
-UDP_IP = "192.168.1.16"  # Reemplázalo con la dirección IP del receptor
-UDP_PORT = 5000     # Reemplázalo con el puerto adecuado
+import time
 
 # Mensaje que se enviará
-MESSAGE = b"Hola desde Python2!"
+Trama1 = b"Hola desde Python21!"
+Trama2 = b"Hola desde Python22!"
+T_Espera= 0.5 #Tiempo en segundos
+
+# Definir la dirección IP y el puerto al que se va a enviar el mensaje
+UDP_IP = "192.168.1.11"  # Dirección IP del receptor
+UDP_PORT = 4000     # Puerto del receptor
 
 # Crear el socket UDP
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
 # Enviar el mensaje
-sock.sendto(MESSAGE, (UDP_IP, UDP_PORT))
+sock.sendto(Trama1, (UDP_IP, UDP_PORT))
+time.sleep(T_Espera)
+sock.sendto(Trama2 , (UDP_IP, UDP_PORT))
 
 print(f"Mensaje enviado a {UDP_IP}:{UDP_PORT}")
 
