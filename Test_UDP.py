d0267ef94ff45e5e60b1a2239ec48baaf032f710
eusmas <eus08@hotmail.com>
2024-09-15 01:51:36 -0500

    se creo funcion de simular a Fosber

--- Test_UDP.py
+++ Test_UDP.py
@@ -1,21 +1,18 @@
 import socket
 import time
-import Fosber
+from GeneradorFosber import SimulacionTramaFosber
 
 # Definir la dirección IP y el puerto al que se va a enviar el mensaje
 UDP_IP = "192.168.0.10"  # Dirección IP del receptor
 UDP_PORT = 2000     # Puerto del receptor
-T_Espera= 3 #Tiempo en segundos
-
-Trama= Fosber.Fosber_temp
-#Trama="LPDID4011MD0SN5073 19 1WBC-C450FLCSI1SMWILOL1BI723115A-01/1BUPRODUCTOS FAMILIACD050031 MEDELLINBW5100BL8680ST850 3400 850BO1BC393SH400SP2BP0ET0TS0PSTLP0CMFF21MNFF21DD20240822PC16047943PW0PL0NW0NL0DO0TB0BD0EP0P1 P2 P3 P4 BATRT0CS03D"
+T_Espera= 1 #Tiempo en segundos
 
 # Crear el socket UDP
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
 #Consecutivo
 for x in range(100):
-    Fosber= "" + f"{x:02}" + Trama
+    Fosber= "" + f"{x:02}" + SimulacionTramaFosber()
     print(Fosber) 
     sock.sendto(Fosber.encode(encoding="utf-8"), (UDP_IP, UDP_PORT))
     time.sleep(T_Espera)
