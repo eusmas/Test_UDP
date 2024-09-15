4909d4f2fa1fb4d2dc06c1a3eb24ac0fef8c9617
eusmas <eus08@hotmail.com>
2024-09-15 14:14:48 -0500

    ajustes varios

--- Test_UDP.py
+++ Test_UDP.py
@@ -5,7 +5,7 @@ from GeneradorFosber import SimulacionTramaFosber
 # Definir la dirección IP y el puerto al que se va a enviar el mensaje
 UDP_IP = "192.168.0.10"  # Dirección IP del receptor
 UDP_PORT = 2000     # Puerto del receptor
-T_Espera= 1 #Tiempo en segundos
+T_Espera= 0.1 #Tiempo en segundos
 
 # Crear el socket UDP
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
