40267a95633d32eb18152df4467c56b210586f02
eusmas <eus08@hotmail.com>
2024-09-16 17:01:21 -0500

    se agrega opcion de enviar un solo numero

--- Test_UDP.py
+++ Test_UDP.py
@@ -1,17 +1,21 @@
 import socket
 import time
+import random
 from GeneradorFosber import SimulacionTramaFosber
+
+num=random.randint(1,100)
 
 # Definir la dirección IP y el puerto al que se va a enviar el mensaje
 UDP_IP = "192.168.0.10"  # Dirección IP del receptor
 UDP_PORT = 2000     # Puerto del receptor
-T_Espera= 0.1 #Tiempo en segundos
+T_Espera= 1 #Tiempo en segundos
 
 # Crear el socket UDP
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
 #Consecutivo
-for x in range(100):
+#for x in range(100):
+for x in range(num ,num+1):   
     Fosber= "" + f"{x:02}" + SimulacionTramaFosber()
     print(Fosber) 
     sock.sendto(Fosber.encode(encoding="utf-8"), (UDP_IP, UDP_PORT))
@@ -20,3 +24,4 @@ for x in range(100):
 print(f"Mensaje enviado a {UDP_IP}:{UDP_PORT}")
 # Cerrar el socket
 sock.close()
+
